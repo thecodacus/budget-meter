4e887cabd2f35fde2baf689cdf87e55329123941
Anirban Kar <thecodacus@gmail.com>
2022-08-08 01:13:01 +0530

    adding capacitor

---
+++ android/app/src/main/java/com/codacus/budgetmeter/MainActivity.java
@@ -0,0 +1,5 @@
+package com.codacus.budgetmeter;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.codacus.budgetmeter',
+  appName: 'budget-meter',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
