f72d1b32f3cde1b72d77c94bcbf15bc404b0650c
Anirban Kar <thecodacus@gmail.com>
2022-08-08 12:18:12 +0530

    removed ios

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.codacus.budgetmeter',
-  appName: 'budget-meter',
+  appName: 'Budget Meter',
   webDir: 'dist',
   bundledWebRuntime: false
 };
